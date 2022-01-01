d7d88068ba086469b0a33304357fa8b8e5386f6d
alingse <alingse@foxmail.com>
2022-01-01 21:43:03 +0800

    add Activity && upgrade android

--- infclip/lib/main.dart
+++ infclip/lib/main.dart
@@ -1,3 +1,4 @@
+/*
 import 'dart:async';
 import 'dart:developer';
 
@@ -92,5 +93,130 @@ class _HomePageState extends State<HomePage> {
               text: TextSpan(
                   text: content, style: Theme.of(context).textTheme.headline6));
         });
+  }
+}
+*/
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  FlutterBackgroundService.initialize(onStart);
+
+  runApp(MyApp());
+}
+
+void onStart() {
+  WidgetsFlutterBinding.ensureInitialized();
+  final service = FlutterBackgroundService();
+  service.onDataReceived.listen((event) {
+    if (event!["action"] == "setAsForeground") {
+      service.setForegroundMode(true);
+      return;
+    }
+
+    if (event["action"] == "setAsBackground") {
+      service.setForegroundMode(false);
+    }
+
+    if (event["action"] == "stopService") {
+      service.stopBackgroundService();
+    }
+  });
+
+  // bring to foreground
+  service.setForegroundMode(true);
+  Timer.periodic(Duration(seconds: 1), (timer) async {
+    if (!(await service.isServiceRunning())) timer.cancel();
+    service.setNotificationInfo(
+      title: "My App Service",
+      content: "Updated at ${DateTime.now()}",
+    );
+
+    service.sendData(
+      {"current_date": DateTime.now().toIso8601String()},
+    );
+  });
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String text = "Stop Service";
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Service App'),
+        ),
+        body: Column(
+          children: [
+            StreamBuilder<Map<String, dynamic>?>(
+              stream: FlutterBackgroundService().onDataReceived,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                final data = snapshot.data!;
+                DateTime? date = DateTime.tryParse(data["current_date"]);
+                return Text(date.toString());
+              },
+            ),
+            ElevatedButton(
+              child: Text("Foreground Mode"),
+              onPressed: () {
+                FlutterBackgroundService()
+                    .sendData({"action": "setAsForeground"});
+              },
+            ),
+            ElevatedButton(
+              child: Text("Background Mode"),
+              onPressed: () {
+                FlutterBackgroundService()
+                    .sendData({"action": "setAsBackground"});
+              },
+            ),
+            ElevatedButton(
+              child: Text(text),
+              onPressed: () async {
+                var isRunning =
+                    await FlutterBackgroundService().isServiceRunning();
+                if (isRunning) {
+                  FlutterBackgroundService().sendData(
+                    {"action": "stopService"},
+                  );
+                } else {
+                  FlutterBackgroundService.initialize(onStart);
+                }
+                if (!isRunning) {
+                  text = 'Stop Service';
+                } else {
+                  text = 'Start Service';
+                }
+                setState(() {});
+              },
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            FlutterBackgroundService().sendData({
+              "hello": "world",
+            });
+          },
+          child: Icon(Icons.play_arrow),
+        ),
+      ),
+    );
   }
 }
